bf2cce3b8ddfc203a476795d20df36d98c2f8932
Ashoka Shringla <ashokashringla@gmail.com>
2022-01-26 14:22:44 -0500

    Added initial commit

---
+++ src/main/java/com/revature/JavalinSample/App.java
@@ -0,0 +1,11 @@
+package com.revature.JavalinSample;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        
+    }
+}
